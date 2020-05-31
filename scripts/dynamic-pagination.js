03bdf1e4bad9e57788c01ecc959e405d2148db30
chollma <53924848+chollma@users.noreply.github.com>
2020-05-30 23:22:15 -0700

    Pagination mostly working

---
+++ scripts/dynamic-pagination.js
@@ -0,0 +1,62 @@
+/******************************************
+Student: Cooper Hollmaier
+Project: Techdegree - Unit 2
+Intended Behavior: List Filter and Pagination
+Goal: Meets Expecatations
+******************************************/
+/* Global Variables */
+const list = document.getElementsByClassName('book-list');
+const display = document.getElementsByClassName('book-list-item');
+const count = 5;
+const div = document.createElement('div');
+const container = document.getElementsByClassName('container');
+
+/* Display the supplied list with the given constraints on page size */
+function showPage(list, page) {
+    const startIndex = (page * count) - count;
+    const endIndex = page * count;
+    for (let i = 0; i < list.length; i++) {
+        if (i >= startIndex && i < endIndex) {
+            list[i].style.display = 'block';
+        } else {
+            list[i].style.display = 'none';
+        }
+    }
+}
+/* Dynamically add pagination links based on record count to bottom of the page */
+function addPagination(list) {
+    container[0].appendChild(div);
+    div.className = "pagination";
+    let ul = document.createElement('ul');
+    div.appendChild(ul);
+    
+
+    for (let i = 0; i < (list.length) / count; i++) {
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        ul.appendChild(li);
+        li.appendChild(a);
+        a.setAttribute('href', '#');
+        a.innerHTML = i + 1;
+        let links = ul.querySelectorAll('a');
+
+
+        ul.addEventListener('click', (e) => {
+            if (e.target.tagName = 'A') {
+                for (let i = 0; i < links.length; i++) {
+                    links[i].className = '';
+                }
+                e.target.className = "active";
+                showPage(list, e.target.innerHTML);
+            }
+
+
+        });
+    }
+    let first = document.querySelector('a');
+    first.className = "active";
+
+}
+
+showPage(list, 1);
+addPagination(display);
