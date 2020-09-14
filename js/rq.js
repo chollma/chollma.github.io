1154a9b91cd50c87d492594ade429a00199e00d2
chollma <53924848+chollma@users.noreply.github.com>
2020-09-13 17:55:03 -0700

    Turned the random quote generator into a random book generator

--- js/rq.js
+++ js/rq.js
@@ -1,60 +1,286 @@
-/******************************************
-Student: Cooper Hollmaier
-Project: Techdegree - Unit 1
-Intended Behavior: A Random Quote Generator
-Goal: Exceeds Expecatations
-******************************************/
+var books = [
 
-// Pre-selected a list of 10 outdoor themed quotes. 2 of them contain values for citation and year. 
-var quotes = [
-    { quote: "The Antidote to exhaustion isn't rest. It's nature.", source: 'Shikoba' },
-    { quote: "Earth and sky, woods and fields, lakes and rivers, the mountain and the sea, are excellent schoolmasters, and teach of us more than we can ever learn from books.", source: 'John Lubbock' },
-    { quote: "He is richest who is content with the least, for content is the wealth of nature.", source: 'Socrates' },
-    { quote: "I took a walk in the woods and came out taller than the trees.", source: 'Henry David Thoreau' },
-    { quote: "The human spirit needs places where nature has not been rearranged by the hand of man.", source: 'Proverb' },
-    { quote: "All good things are wild and free.", source: 'Proverb' },
-    { quote: "In every walk with nature one receives far more than he seeks.", source: 'John Muir', citation: 'Steep Trails', year: '1918' },
-    { quote: "I'd rather be in the mountains thinking of God than in church thinking about the mountains.", source: 'John Muir', citation: 'The Unpublished Journals of John Muir', year: '1924' },
-    { quote: "Because in the end, you won't remember the time you spent working in the office or mowing your lawn. Climb that goddamn mountain.", source: 'Jack Kerouac' },
-    { quote: "I go to nature every day for inspiration in the day's work.", source: 'Frank Lloyd Wright' }
-]
+    {
+        uri: "kca://book/amzn1.gr.book.v1.4GslP_VRrOSYQg5G9eG6Uw",
+        title: "American Ground: Unbuilding the World Trade Center",
+        image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+        num_pages: "240",
+        publication_day: "11",
+        publication_year: "2003",
+        publication_month: "9",
+        average_rating: "4.09",
+        ratings_count: "727",
+        name: "William Langewiesche"
+    },
 
-// Pre-selected list that contains the colors of the rainbow as strings of text
-var colors = [
-    { color: '#838181' },
-    { color: '#303030' },
-    { color: '#405E58' },
-    { color: '#1F739D' },
-    { color: '#8A3222' },
-    { color: '#1DA1F2' },
-    { color: '#3977f2' }
+    {
+        uri: "kca://book/amzn1.gr.book.v1.t6leceM1iDR9x1AenJCETg",
+        title: "Automate the Boring Stuff with Python: Practical Programming for Total Beginners",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1418768948l/22514127._SX98_.jpg",
+        num_pages: "479",
+        publication_day: "1",
+        publication_year: "2015",
+        publication_month: "5",
+        average_rating: "4.28",
+        ratings_count: "1668",
+        name: "Al Sweigart"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.OYgHo_97kTmGbedB1chEww",
+        title: "Barbarian Days: A Surfing Life",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1418768620l/18693910._SX98_.jpg",
+        num_pages: "447",
+        publication_day: "21",
+        publication_year: "2015",
+        publication_month: "7",
+        average_rating: "4.23",
+        ratings_count: "18329",
+        name: "William Finnegan",
+    },
+
+    {
+        title: "The Battle for God: A History of Fundamentalism",
+        image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+        num_pages: "480",
+        publication_day: "30",
+        publication_year: "2001",
+        publication_month: "1",
+        average_rating: "3.94",
+        ratings_count: "6132",
+        name: "Karen Armstrong"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.aQ5_XCrG7-BaO1seB5oz2g",
+        title: "Behind the Beautiful Forevers: Life, Death, and Hope in a Mumbai Undercity",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1315601232l/11869272._SX98_.jpg",
+        num_pages: "278",
+        publication_day: "7",
+        publication_year: "2012",
+        publication_month: "2",
+        average_rating: "3.98",
+        ratings_count: "97895",
+        name: "Katherine Boo"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.UseBB3vJRioqzpKRKviEkA",
+        title: "Bird by Bird: Some Instructions on Writing and Life",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1394996112l/12543._SX98_.jpg",
+        num_pages: "237",
+        publication_day: "1",
+        publication_year: "1995",
+        publication_month: "9",
+        average_rating: "4.23",
+        ratings_count: "77147",
+        name: "Anne Lamott"
+
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.TpbKN9n2AC4OZrHF9eaOMg",
+        title: "Borrowed Finery: A Memoir",
+        image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+        num_pages: "224",
+        publication_day: "12",
+        publication_year: "2005",
+        publication_month: "11",
+        average_rating: "3.54",
+        ratings_count: "631",
+        name: "Paula Fox"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.Lj-RGgh144BNI79DM7pQqQ",
+        title: "Can't Stop Won't Stop: A History of the Hip-Hop Generation",
+        image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+        num_pages: "546",
+        publication_day: "27",
+        publication_year: "2005",
+        publication_month: "12",
+        average_rating: "4.15",
+        ratings_count: "4509",
+        name: "Jeff Chang"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v3.l6MimVey6QTnZq34",
+        title: "Data Feminism",
+        image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+        num_pages: "",
+        publication_day: "",
+        publication_year: "",
+        publication_month: "",
+        average_rating: "4.53",
+        ratings_count: "111",
+        name: "Catherine D’Ignazio"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.-QIqCKRd4pN6T1_8icz60Q",
+        title: "David and Goliath: Underdogs, Misfits, and the Art of Battling Giants",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1391813567l/15751404._SX98_.jpg",
+        num_pages: "305",
+        publication_day: "1",
+        publication_year: "2013",
+        publication_month: "10",
+        average_rating: "3.95",
+        ratings_count: "146032",
+        name: "Malcolm Gladwell"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.mTTUhAtTsUgyd3cy3Rs0Zw",
+        title: "Decoding the Social World: Data Science and the Unintended Consequences of Communication",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1575129844l/34540106._SX98_.jpg",
+        num_pages: "256",
+        publication_day: "22",
+        publication_year: "2017",
+        publication_month: "12",
+        average_rating: "4.20",
+        ratings_count: "10",
+        name: "Sandra González-Bailón"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.K3joXTEgP237Z1pvtDKQow",
+        title: "Doughnut Economics: Seven Ways to Think Like a 21st-Century Economist",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1487215696l/29214420._SX98_.jpg",
+        num_pages: "384",
+        publication_day: "6",
+        publication_year: "2017",
+        publication_month: "4",
+        average_rating: "4.22",
+        ratings_count: "4583",
+        name: "Kate Raworth"
+    },
+
+    {
+        uri: "kca://book/amzn1.gr.book.v1.LkA08ysfgwrdlR3hWuPD-w",
+        title: "The Electric Kool-Aid Acid Test",
+        image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+        num_pages: "416",
+        publication_day: "",
+        publication_year: "",
+        publication_month: "",
+        average_rating: "3.92",
+        ratings_count: "68560",
+        name: "Tom Wolfe",
+    },
+    {
+        uri: "kca://book/amzn1.gr.book.v1.VGEWhWnXlZqYmEtWKlIicQ",
+        title: "Far from the Tree: Parents, Children, and the Search for Identity",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554787839l/13547504._SX98_.jpg",
+        num_pages: "962",
+        publication_day: "13",
+        publication_year: "2012",
+        publication_month: "11",
+        average_rating: "4.24",
+        ratings_count: "17954",
+        name: "Andrew Solomon"
+    },
+    {
+        uri: "kca://book/amzn1.gr.book.v1.fuN7QbRXoyHrGCgZqUfelw",
+        title: "Field Notes from a Catastrophe: Man, Nature, and Climate Change",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1437525015l/80513._SX98_.jpg",
+        num_pages: "225",
+        publication_day: "1",
+        publication_year: "2007",
+        publication_month: "1",
+        average_rating: "4.01",
+        ratings_count: "3483",
+        name: "Elizabeth Kolbert"
+    },
+    {
+        uri: "kca://book/amzn1.gr.book.v1.bpBNlmCHtoSedaGJTlJs4g",
+        title: "Finishing the Hat: Collected Lyrics, 1954-1981, With Attendant Comments, Principles, Heresies, Grudges, Whines, and Anecdotes",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1320542554l/7769406._SX98_.jpg",
+        num_pages: "421",
+        publication_day: "26",
+        publication_year: "2010",
+        publication_month: "10",
+        average_rating: "4.52",
+        ratings_count: "1717",
+        name: "Stephen Sondheim"
+    },
+    {
+        uri: "kca://book/amzn1.gr.book.v1.mopzLCLT1T2BtT0vsIPgbg",
+        title: "Fun Home: A Family Tragicomic",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1440097020l/26135825._SX98_.jpg",
+        num_pages: "232",
+        publication_day: "5",
+        publication_year: "2007",
+        publication_month: "6",
+        average_rating: "4.09",
+        ratings_count: "128660",
+        name: "Alison Bechdel"
+    },
+    {
+        uri: "kca://book/amzn1.gr.book.v1.XHofrwMLl0s08LaoHv8zmA",
+        title: "Going Clear: Scientology, Hollywood, and the Prison of Belief",
+        image_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1358119149l/16142053._SX98_.jpg",
+        num_pages: "430",
+        publication_day: "17",
+        publication_year: "2013",
+        publication_month: "1",
+        average_rating: "4.03",
+        ratings_count: "38102",
+        name: "Lawrence Wright"
+    },
+    {
+        uri: "kca://book/amzn1.gr.book.v1.8S8Li0EZOc5WqtEIi9UO4w",
+        title: "H is for Hawk",
+        image_url: "https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png",
+        num_pages: "300",
+        publication_day: "31",
+        publication_year: "2014",
+        publication_month: "7",
+        average_rating: "3.73",
+        ratings_count: "57638",
+        name: "Helen Macdonald"
+    },
 ]
 
 // Generation of a random number to serve as the index for accessing the object above. Output should be a singular item object in the array.
-function getRandomQuote(array) {
+function getRandomObject(array) {
     var number = Math.floor((Math.random() * array.length));
     var output = array[number];
     return output;
 }
 
+
+
 function getRandomColor(array) {
-    var numberTwo = Math.floor((Math.random() * array.length));
-    var outputColor = array[numberTwo];
+    var red = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var outputColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
     return outputColor;
 
 }
 
 // Assembly of HTML output string utilizing the information returned from getRandomQuote
-setInterval(function printQuote() {
-    var responseQuote = getRandomQuote(quotes); // Get a random quote object with properties
-    var responseColor = getRandomColor(colors).color; // Get random color object with properties
-    var outputHTML = '<p class="quote">' + responseQuote.quote + '</p>';
-    outputHTML += '<p class="source">' + responseQuote.source;
+function printQuote() {
 
-    if (responseQuote.citation) {
-        outputHTML += ' <span class="citation">' + responseQuote.citation + '</span>';
-    }
+    // Get elements of the randomly chosen object
+    var response = getRandomObject(books); // Get a random quote object with properties
+
+    var outputHTML = '<p class="quote"><strong>' + response.title + '</strong></p>';
+    outputHTML += '<p>' + response.name + '</p>'
+    outputHTML += '<p>' + response.num_pages + ' pages</p>'
+    outputHTML += '<p class="source">' + 'Published: ' + response.publication_month + '/' + response.publication_day + '/' + response.publication_year;
+    outputHTML += '<p>Average rating: ' + response.average_rating + ' from ' + response.ratings_count + ' reviews</p>'
     outputHTML += '</p>';
     document.getElementById('quote-box').innerHTML = outputHTML;
-    document.getElementById('quote-box').style.boxShadow = '0 4px 8px 0 ' + responseColor;
-}, 10000);
+
+
+
+    // Change Color of Box Shadow    
+    var responseColor = getRandomColor(); // Get random color object with properties
+    document.getElementById('quote-box').style.boxShadow = '0 0.25rem 0.5rem 0 ' + responseColor;
+}
+
+console.log(books)
+
+// TODO -  can I feed these quotes from somewhere else (API)
